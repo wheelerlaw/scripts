50d4da78e5bbd3cadb0e7781039b187a662ca0e7
Wheeler Law <wheeler@redhat.com>
2019-09-16 02:10:20 -0400

    Working through bugs

--- amc.groovy
+++ amc.groovy
@@ -194,7 +194,7 @@ def extract(f) {
 	}
 
 //	def folder = new File(extractFolder ?: f.dir, f.nameWithoutExtension)
-	def folder = new File(extractFolder ?: f.dir)
+	def folder = new File(extractFolder ?: f.dir.name)
 //	def files = extract(file: f, output: folder.resolve(f.dir.name), conflict: 'auto', filter: { it.isArchive() || it.isVideo() || it.isSubtitle() || (music && it.isAudio()) }, forceExtractAll: true) ?: []
 	def files = extract(file: f, output: folder, conflict: 'auto', filter: { it.isArchive() || it.isVideo() || it.isSubtitle() || (music && it.isAudio()) }, forceExtractAll: true) ?: []
 
